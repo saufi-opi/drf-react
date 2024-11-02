84d64137f1ed249138cfd0eb678be42e1711e8a1
saufi <saufi1115@gmail.com>
2024-11-02 11:09:01 +0800

    feat: authpage protect

--- frontend/src/pages/AuthPage.tsx
+++ frontend/src/pages/AuthPage.tsx
@@ -1,7 +1,17 @@
 import SigninForm from '@/components/forms/SigninForm'
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card'
+import { useAuth } from '@/hooks/use-auth'
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export default function AuthPage() {
+  const auth = useAuth()
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    if (auth.isAuthenticated && !auth.loading) navigate('/', { replace: true })
+  }, [auth, navigate])
+
   return (
     <div className="flex min-h-screen items-center justify-center">
       <Card className="w-full max-w-md">
